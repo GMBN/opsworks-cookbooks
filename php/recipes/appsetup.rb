e9f6235198f07a7824d45aa598654cce4aad35b3
Gian Nunes <gian@btitecnologia.com>
2016-02-26 00:46:20 -0300

    Create appsetup.rb

---
+++ php/recipes/appsetup.rb
@@ -0,0 +1,12 @@
+node[:deploy].each do |app_name, deploy|
+
+  script "install_composer" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/"
+    code <<-EOH
+    curl -sS https://getcomposer.org/installer | php
+    php composer.phar install --no-dev
+    EOH
+  end
+end
