41099d509a621b7260a954103fe32b9f10fe6719
Gian Nunes <gian@btitecnologia.com>
2016-02-26 22:14:51 -0300

    Update appsetup.rb

--- php/recipes/appsetup.rb
+++ php/recipes/appsetup.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |app_name, deploy|
 
-  template "#{deploy[:deploy_to]}/current/config/autoload/db.local.php" do
+  template "#{deploy[:deploy_to]}/current/config/autoload/db.global.php" do
     source "db-connect.php.erb"
     mode 0660
     group deploy[:group]
@@ -20,7 +20,7 @@ node[:deploy].each do |app_name, deploy|
     )
 
    only_if do
-     File.directory?("#{deploy[:deploy_to]}/current")
+     File.directory?("#{deploy[:deploy_to]}/current/config/autoload")
    end
   end
 end
