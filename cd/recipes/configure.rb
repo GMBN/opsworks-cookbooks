d2898c19ddb032e31eabb43be35f65303b279958
Gian Nunes <gian@btitecnologia.com>
2016-02-29 04:08:29 -0300

    Update configure.rb

--- cd/recipes/configure.rb
+++ cd/recipes/configure.rb
@@ -1,6 +1,8 @@
 node[:deploy].each do |app_name, deploy|
-  directory "#{deploy[:deploy_to]}/current/data" do
+  directory "#{deploy[:deploy_to]}/current/data/DoctrineORMModule/Proxy" do
     mode 0775
+    user deploy[:user]
+    group deploy[:group]
     recursive true
     action :create
   end
